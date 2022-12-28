59ee3fe7d099584920f7fb42c42b89e6652315f3
vaceslavkameronov <slka@mail.ru>
2022-12-28 22:47:34 +0400

    1 задача

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    int dog = 25000;
+    System.out.println("Значение переменной" + " dog" + " с типом" + " int " + "равно "  +  dog);
+    byte cat = 5;
+    System.out.println( "Значение переменной cat с типом byte равно " + cat);
+    short parrot = 1000;
+    System.out.println("Значение переменной parrot с типом shourt равно " + parrot);
+    long butterfly=100000;
+    System.out.println("Значение переменной butterfly с типом shourt равно " + butterfly);
+    float  width=5.6f;
+    System.out.println("Значение переменной width с типом float равно " + width);
+    double length=10.2f;
+    System.out.println("Значение переменной length с типом shourt равно " + length);}
+
+
+}
