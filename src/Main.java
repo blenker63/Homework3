da542db0cf720fa5f4286a407e588cb1057fdc9a
vaceslavkameronov <slka@mail.ru>
2022-12-30 19:37:58 +0400

    выполнена задача 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 public static  void task1() {
     System.out.println("Задача 1");
@@ -12,13 +13,13 @@ public static  void task1() {
     byte catSmall = 5;
     System.out.println("Значение переменной catSmall с типом byte равно " + catSmall);
     short parrot = 1000;
-    System.out.println("Значение переменной parrot с типом shourt равно " + parrot);
+    System.out.println("Значение переменной parrot с типом short равно " + parrot);
     long butterfly=100000;
-    System.out.println("Значение переменной butterfly с типом shourt равно " + butterfly);
+    System.out.println("Значение переменной butterfly с типом short равно " + butterfly);
     float  width=5.6f;
     System.out.println("Значение переменной width с типом float равно " + width);
     double length=10.2f;
-    System.out.println("Значение переменной length с типом shourt равно " + length);}
+    System.out.println("Значение переменной length с типом short равно " + length);}
     public static  void task2() {
         System.out.println("Задача 2");
     float a=27.12f;
@@ -48,20 +49,33 @@ public static  void task1() {
         System.out.println("Задача 4");
     byte bottle = 16;
     byte timeMin = 2;
-    byte perfomanceBottleTime = (byte) (bottle/timeMin); //расчет производительности за 1 минуту
+    byte performanceBottleTime = (byte) (bottle/timeMin); //расчет производительности за 1 минуту
     byte OneSpecifiedIntervalMinute = 20;
     byte TwoSpecifiedIntervalDay = 1;
     byte ThreeSpecifiedIntervalDay = 3;
     byte FourSpecifiedIntervalMonth = 1;
-    short OnePerfomanceBottle = (short) (perfomanceBottleTime * OneSpecifiedIntervalMinute);
-    System.out.println("За " + OneSpecifiedIntervalMinute + " минут машина произвела " + OnePerfomanceBottle + " штук бутылок.");
-    short TwoSpecifiedIntervalMinute = (short) (TwoSpecifiedIntervalDay * 24 * 60);
-    short TwoPerfomanceBottle = (short) (TwoSpecifiedIntervalMinute * perfomanceBottleTime);
-    System.out.println("За " + TwoSpecifiedIntervalDay + " сутки машина произвела " + TwoPerfomanceBottle + " штук бутылок.");
-    short ThreeSpecifiedIntervalMinute = (short) (ThreeSpecifiedIntervalDay * 24 * 60);
-    int ThreePerfomanceBottle = (int) (ThreeSpecifiedIntervalMinute * perfomanceBottleTime);
-    System.out.println("За " + ThreeSpecifiedIntervalDay + " дня машина произвела " + ThreePerfomanceBottle + " штук бутылок.");
-    int FourSpecifiedIntervalMinute = (int) (FourSpecifiedIntervalMonth * 30 * 24 * 60);
-    int FourPerfomanceBottle = (int) (FourSpecifiedIntervalMinute * perfomanceBottleTime);
-    System.out.println("За " + FourSpecifiedIntervalMonth + " месяц машина произвела " + FourPerfomanceBottle + " штук бутылок.");}
+    byte minuteHour = 60; // количество минут в часе
+    byte hourDay = 24; // количество часов в сутках
+    byte dayMonth = 30; // количество дней в месяце
+    short OnePerformanceBottle = (short) (performanceBottleTime * OneSpecifiedIntervalMinute);
+    System.out.println("За " + OneSpecifiedIntervalMinute + " минут машина произвела " + OnePerformanceBottle + " штук бутылок.");
+    short TwoSpecifiedIntervalMinute = (short) (TwoSpecifiedIntervalDay * hourDay * minuteHour);
+    short TwoPerformanceBottle = (short) (TwoSpecifiedIntervalMinute * performanceBottleTime);
+    System.out.println("За " + TwoSpecifiedIntervalDay + " сутки машина произвела " + TwoPerformanceBottle + " штук бутылок.");
+    short ThreeSpecifiedIntervalMinute = (short) (ThreeSpecifiedIntervalDay * hourDay * minuteHour);
+    int ThreePerformanceBottle = (short) (ThreeSpecifiedIntervalMinute * performanceBottleTime);
+    System.out.println("За " + ThreeSpecifiedIntervalDay + " дня машина произвела " + ThreePerformanceBottle + " штук бутылок.");
+    int FourSpecifiedIntervalMinute = (short) (FourSpecifiedIntervalMonth * dayMonth * hourDay * minuteHour);
+    int FourPerformanceBottle = (short) (FourSpecifiedIntervalMinute * performanceBottleTime);
+    System.out.println("За " + FourSpecifiedIntervalMonth + " месяц машина произвела " + FourPerformanceBottle + " штук бутылок.");}
+    public static  void task5() {
+        System.out.println("Задача 5");
+    byte potTotal = 120;
+    byte oneClassPotWhite = 2;
+    byte oneClassPotBrown = 4;
+    byte oneClassPotTotal = (byte) (oneClassPotBrown + oneClassPotWhite); // Сколько банок красок уходит на 1 класс
+    byte totalClassSchool = (byte) (potTotal / oneClassPotTotal); // Всего классов в школе
+    byte potTotalWhite = (byte) (totalClassSchool * oneClassPotWhite); // сколько банок белой краски на 1 класс
+    byte potTotalBrown = (byte) (totalClassSchool * oneClassPotBrown); // сколько банок коричневой краски на 1 класс
+    System.out.println("В школе, где " + totalClassSchool + " классов, нужно " + potTotalWhite + " банок белой краски и " + potTotalBrown + " банок коричневой краски.");}
 }
